288699fb57332c3966feaa849dbfaa053f1c5201
klich84 <klich84@gmail.com>
2025-06-17 11:49:33 -0500

    chore: restruct fonts and add new font

---
+++ src/app/config/fonts.ts
@@ -0,0 +1,22 @@
+import { Geist, Geist_Mono, Montserrat_Alternates, Raleway } from 'next/font/google'
+
+export const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+export const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const titleFont = Montserrat_Alternates({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+  weight: ['500', '700'],
+})
+
+export const releway = Raleway({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
