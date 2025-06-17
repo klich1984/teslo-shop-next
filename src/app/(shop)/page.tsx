ce98d7144b41328421c3c82e6a0cfa74fc861b63
klich84 <klich84@gmail.com>
2025-06-17 12:31:29 -0500

    chore(structure): init folder setup and base pages

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,11 @@
-import { titleFont } from '../config/fonts'
+import { titleFont } from '../../config/fonts'
 
 export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main>
-        <h1>Hello!</h1>
+      <div>
         <h1 className={titleFont.className}>Hello!</h1>
-      </main>
+      </div>
     </div>
   )
 }
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from '../../../config/fonts'
+
+export default function Home() {
+  return (
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <div>
+        <h1 className={titleFont.className}>Login Page</h1>
+      </div>
+    </div>
+  )
+}
