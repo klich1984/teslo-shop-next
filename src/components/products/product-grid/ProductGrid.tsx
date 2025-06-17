57192a8370544c3838112fab70e5d738454d1a64
klich84 <klich84@gmail.com>
2025-06-17 18:26:01 -0500

    feat: 🗿 Add ProductGrid Component

---
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -0,0 +1,15 @@
+import { Product } from '@/interfaces'
+
+interface ProductGridProps {
+  products: Product[]
+}
+
+export const ProductGrid = ({ products }: ProductGridProps) => {
+  return (
+    <div className='grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10'>
+      {products.map((product) => (
+        <span key={product.slug}>{product.title}</span>
+      ))}
+    </div>
+  )
+}
