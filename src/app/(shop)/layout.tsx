c78fd1246140742012855ca4c9c547063b6717e6
klich84 <klich84@gmail.com>
2025-06-17 15:09:38 -0500

    feat(routes): setup base app routes

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,3 +1,3 @@
 export default function ShopLayout({ children }: { children: React.ReactNode }) {
-  return <main className='bg-blue-500'>{children}</main>
+  return <main className='min-h-screen bg-blue-500'>{children}</main>
 }
