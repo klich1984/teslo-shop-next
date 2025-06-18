d86265f8e04fe38100ca20bcab50c31a21e66a77
klich84 <klich84@gmail.com>
2025-06-18 16:47:54 -0500

    chore: Implement view category Dinamic and change name of types

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,39 @@
-import { notFound } from 'next/navigation'
+// import { notFound } from 'next/navigation'
+
+import { ProductGrid, Title } from '@/components'
+import { Category } from '@/interfaces'
+import { initialData } from '@/seed/seed'
 
 interface CategoryPageProps {
   params: Promise<{
-    id: string
+    id: Category
   }>
 }
+const products = initialData.products
 
 export default async function CategoryPage(props: CategoryPageProps) {
   const params = await props.params
   const { id } = params
+  const filterProducts = products.filter((product) => product.gender === id)
 
-  if (id === 'kids') {
-    notFound()
+  // if (id === 'kids') {
+  //   notFound()
+  // }
+  const label: Record<Category, string> = {
+    men: 'Hombres',
+    women: 'Mujeres',
+    kid: 'Niños',
+    unisex: 'Todos',
   }
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title
+        title={id.toUpperCase()}
+        subtitle={`Artículos para ${label[id]}`}
+        className='mb-2'
+      />
+      <ProductGrid products={filterProducts} />
+    </>
   )
 }
