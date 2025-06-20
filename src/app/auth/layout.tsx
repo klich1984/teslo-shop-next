445fac2a2c7a7c710d6fe9a6eacafd20e7cdf263
klich84 <klich84@gmail.com>
2025-06-20 17:58:55 -0500

    feat: 🗿 Add login and new account Page

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,7 @@
 export default function ShopLayout({ children }: { children: React.ReactNode }) {
-  return <main className='bg-gray-500'>{children}</main>
+  return (
+    <main className='flex justify-center'>
+      <div className='w-full sm:w-[350px] px-10'>{children}</div>
+    </main>
+  )
 }
--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,10 +1,30 @@
+import Link from 'next/link'
 import { titleFont } from '../../../config/fonts'
 
 export default function NewAccount() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <div>
-        <h1 className={titleFont.className}>New Account</h1>
+    <div className='flex flex-col min-h-screen pt-32 sm:pt-52'>
+      <h1 className={`${titleFont.className} text-4xl mb-5`}>Nueva cuenta</h1>
+
+      <div className='flex flex-col'>
+        <label htmlFor='name'>Nombre completo</label>
+        <input className='px-5 py-2 border bg-gray-200 rounded mb-5' type='text' />
+
+        <label htmlFor='email'>Contraseña</label>
+        <input className='px-5 py-2 border bg-gray-200 rounded mb-5' type='email' />
+
+        <button className='btn-primary'>Crear cuenta</button>
+
+        {/* divisor l ine */}
+        <div className='flex items-center my-5'>
+          <div className='flex-1 border-t border-gray-500'></div>
+          <div className='px-2 text-gray-800'>O</div>
+          <div className='flex-1 border-t border-gray-500'></div>
+        </div>
+
+        <Link href='/auth/login' className='btn-secondary text-center'>
+          Ingresar
+        </Link>
       </div>
     </div>
   )
