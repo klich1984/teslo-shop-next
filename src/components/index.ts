82fbed25bd46434ea3dd3e47010c2aa769b6e847
klich84 <klich84@gmail.com>
2025-06-20 17:47:23 -0500

    feat: 🗿 Add footer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export * from './product/slidesshow/ProductSlidesshow'
 export * from './products/product-grid/ProductGrid'
 export * from './products/product-grid/ProductGridItem'
 
+export * from './ui/footer/Footer'
 export * from './ui/not-found/PageNotFound'
 export * from './ui/sidebar/Sidebar'
 export * from './ui/title/Title'
