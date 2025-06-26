feb7d1ea5a0d6d05dc6d6fdc7adf8f6dfb25b056
klich84 <klich84@gmail.com>
2025-06-26 10:46:09 -0500

    🚨 fix(pdp): show error message if no size is selected
    - Added `<span>` tag to display error message
    - Implemented logic to validate size selection

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -11,9 +11,22 @@ interface AddToCartProps {
 export const AddToCart = ({ product }: AddToCartProps) => {
   const [size, setSize] = useState<Size | undefined>()
   const [quantity, setQuantity] = useState<number>(1)
+  const [posted, setPosted] = useState(false)
+
+  const addToCart = () => {
+    setPosted(true)
+    if (!size) return
+
+    console.log('👽 addToCartt', { size, quantity })
+  }
 
   return (
     <>
+      {posted && !size && (
+        <span className='mt-2 text-red-500 fade-in fade-out'>
+          Debe de seleccionar una talla
+        </span>
+      )}
       {/* Selector de talla */}
       <SizeSelector
         availableSizes={product.sizes}
@@ -23,7 +36,9 @@ export const AddToCart = ({ product }: AddToCartProps) => {
       {/* Selector de cantidad */}
       <QuantitySelector quantity={quantity} onQuantityChanged={setQuantity} />
       {/* Boton */}
-      <button className='btn-primary my-5'>Agregar al carrito</button>
+      <button className='btn-primary my-5' onClick={addToCart}>
+        Agregar al carrito
+      </button>
     </>
   )
 }
