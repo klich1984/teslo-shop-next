5027edd2cb09fc63af50f01e25a50043691971a4
klich84 <klich84@gmail.com>
2025-07-09 18:01:53 -0500

    feat(server): ☁️ add order totals logic to server action

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from '@/auth.config'
 import type { Address, Size } from '@/interfaces'
+import { prisma } from '@/lib/prisma'
 
 interface ProductToOrder {
   productId: string
@@ -14,6 +15,7 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
 
   const userId = session?.user.id
 
+  // verificar sesión de usuario
   if (!userId) {
     return {
       ok: false,
@@ -21,5 +23,37 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
     }
   }
 
-  console.log('👽 ~ placeOlder ~ productIds:', { productIds, address, userId })
+  // Obtener la información de los productos
+  // Nota: Recordar que podemos llevar 2 o + productos con el mismo id
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((p) => p.productId),
+      },
+    },
+  })
+
+  // Calcular los montos // Encabezado
+  const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0)
+
+  // Los totales de tax, subtotal y total
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity
+      const product = products.find((product) => product.id === item.productId)
+
+      if (!product) throw new Error(`${item.productId} no existe - 500`)
+
+      const subTotal = product.price * productQuantity
+
+      totals.subTotal += subTotal
+      totals.tax += subTotal * 0.15
+      totals.total += subTotal * 1.15
+
+      return totals
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+  )
+
+  // Crear la transacción de base de datos
 }
