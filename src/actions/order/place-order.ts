2dc656623add0166fce4c6b1cdcc75a151dfc5c6
klich84 <klich84@gmail.com>
2025-07-10 15:21:05 -0500

    feat(db): 📍 save order address using prisma.$transaction

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -79,12 +79,22 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
         },
       },
     })
+    // Validar si el price es 0 lanzando un error.
+
     // 3. Crear la dirección de la orden
+    const { country, ...restAddress } = address
+    const orderAddress = await tx.orderAddress.create({
+      data: {
+        ...restAddress,
+        countryId: country,
+        orderId: order.id,
+      },
+    })
 
     return {
       order,
       updatedProducts: [],
-      orderAddress: {},
+      orderAddress,
     }
   })
 }
