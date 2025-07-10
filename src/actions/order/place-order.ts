a87e8eea0d00330c0472b80a99a03d93acb874bf
klich84 <klich84@gmail.com>
2025-07-10 15:52:51 -0500

    feat(db): 📦 sync inventory with prisma.$transaction

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -56,45 +56,89 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
   )
 
   // Crear la transacción de base de datos
-  const prismaTx = await prisma.$transaction(async (tx) => {
-    // 1. Actualizar el stock de los productos
-    // 2. Crear la orden - Encabezado - Detalles
-    const order = await tx.order.create({
-      data: {
-        userId,
-        itemsInOrder,
-        subTotal,
-        tax,
-        total,
+  try {
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      // 1. Actualizar el stock de los productos
+      const updatedProductsPromises = products.map((product) => {
+        // Acumular los valores
+        const productQuantity = productIds
+          .filter((p) => p.productId === product.id)
+          .reduce((acc, item) => item.quantity + acc, 0)
 
-        OrderItem: {
-          createMany: {
-            data: productIds.map((p) => ({
-              quantity: p.quantity,
-              size: p.size,
-              productId: p.productId,
-              price: products.find((prod) => prod.id === p.productId)?.price ?? 0,
-            })),
+        if (productQuantity === 0) {
+          throw new Error(`${product.id} no tiene cantidad definida`)
+        }
+
+        return tx.product.update({
+          where: { id: product.id },
+          data: {
+            // inStock: product.inStock -productQuantity => No hacer por que el valor puede estar desfasado
+            inStock: {
+              decrement: productQuantity,
+            },
+          },
+        })
+      })
+
+      const updatedProducts = await Promise.all(updatedProductsPromises)
+
+      // Verificar valores negativos en las existencia = No hay stock
+      updatedProducts.forEach((product) => {
+        if (product.inStock < 0) {
+          throw new Error(`${product.title} no tiene inventario suficiente`)
+        }
+      })
+
+      // 2. Crear la orden - Encabezado - Detalles
+      const order = await tx.order.create({
+        data: {
+          userId,
+          itemsInOrder,
+          subTotal,
+          tax,
+          total,
+
+          OrderItem: {
+            createMany: {
+              data: productIds.map((p) => ({
+                quantity: p.quantity,
+                size: p.size,
+                productId: p.productId,
+                price: products.find((prod) => prod.id === p.productId)?.price ?? 0,
+              })),
+            },
           },
         },
-      },
-    })
-    // Validar si el price es 0 lanzando un error.
+      })
+      // Validar si el price es 0 lanzando un error.
 
-    // 3. Crear la dirección de la orden
-    const { country, ...restAddress } = address
-    const orderAddress = await tx.orderAddress.create({
-      data: {
-        ...restAddress,
-        countryId: country,
-        orderId: order.id,
-      },
+      // 3. Crear la dirección de la orden
+      const { country, ...restAddress } = address
+      const orderAddress = await tx.orderAddress.create({
+        data: {
+          ...restAddress,
+          countryId: country,
+          orderId: order.id,
+        },
+      })
+
+      return {
+        order,
+        updatedProducts,
+        orderAddress,
+      }
     })
 
     return {
-      order,
-      updatedProducts: [],
-      orderAddress,
+      ok: true,
+      order: prismaTx.order,
+      prismaTx,
     }
-  })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return {
+      ok: false,
+      message: error?.message,
+    }
+  }
 }
