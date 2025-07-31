487e309bdded1963fd889f7a5272a70805e24cd5
klich84 <klich84@gmail.com>
2025-07-31 12:45:06 -0500

    feat(product): ✏️ update product with Prisma transaction

--- src/actions/products/create-uspdate-product.ts
+++ src/actions/products/create-uspdate-product.ts
@@ -1,6 +1,7 @@
 'use server'
 
-import { Gender } from '@/generated/prisma'
+import { Gender, Product, Size } from '@/generated/prisma'
+import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 
 const productSchema = z.object({
@@ -34,8 +35,39 @@ export const createUpdateProduct = async (formData: FormData) => {
     }
   }
 
-  console.log('👽 ~ productParsed ~ formData:', productParsed.data)
+  const product = productParsed.data
 
+  product.slug = product.slug.toLowerCase().replace(/ /g, '-').trim()
+
+  const { id, ...rest } = product
+  const prismaTx = await prisma.$transaction(async (tx) => {
+    let productUpdate : Product
+    const tagsArray = product.tags.split(',').map(tag => tag.trim())
+
+    if (id) {
+      // Actualizar
+      productUpdate = await tx.product.update({ // Esta diferente al video
+        where: { id },
+        data: {
+          ...rest,
+          sizes: {
+            set: rest.sizes as Size[],
+          },
+          tags: {
+            set: tagsArray,
+          }
+        }
+      })
+
+      console.log('👽 ProductUpdate', productUpdate)
+    } else {
+      // crear
+    }
+
+    return {}
+  })
+
+  // Todo: revalidate path
   return {
     ok: true,
   }
